e6c534111d08113900ebbdb98300417475948284
Kaitlyn Sierra Cason <cason@lnxsrv02.seas.ucla.edu>
2016-03-10 19:11:53 -0800

    Done?

--- Part 2/SortedList.c
+++ Part 2/SortedList.c
@@ -128,8 +128,7 @@ int SortedList_length(SortedList_t *list)
 
     for(i = 0; i < num_lists; ++i)
     {
-        //curNode = &list[i];
-        SortedList_t* = &list[i];
+        curNode = &list[i];
         if (curNode == NULL)
             break;
 
